8c760dfe769503aaec3143b258ed2b03657f5a5b
user <peyanshu.verma.10@gamil.com>
2025-01-13 15:41:19 +0530

    first commit

--- script.js
+++ script.js
@@ -1,131 +1,120 @@
-// function loco() {
-//           gsap.registerPlugin(ScrollTrigger);
-      
-      
-//           const locoScroll = new LocomotiveScroll({
-//               el: document.querySelector("#main"),
-//               smooth: true
-//           });
-//           locoScroll.on("scroll", ScrollTrigger.update);
-      
-//           ScrollTrigger.scrollerProxy("#main", {
-//               scrollTop(value) {
-//                   return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//               }, 
-//               getBoundingClientRect() {
-//                   return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-//               },
-//               pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-//           });
-      
-//           ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-      
-//           ScrollTrigger.refresh();
-      
-//       }
-      
-//       loco()
+function page1animation() {
 
-gsap.registerPlugin(ScrollTrigger);
-
-function page1animation(){
+  gsap.to('.castle1', {
+    x: -150,
+    scrollTrigger: {
+      trigger: "#main1",
+      start: "top 50",
+      end: "+=" + window.innerHeight * 2,
+      scrub: 1.2,
+    }
+  });
+}
 
 
-gsap.to('.castle1', {
-          x: -150, // Animation property
-          scrollTrigger: {
-            trigger: "#page1",
-            start: "top 50",
-            end: "+=" + window.innerHeight * 2, // Dynamically adjust the end point
-            scrub: 1.2, // Smooth scrubbing
-          }
-        });
-}        
+gsap.to('#logo2', {
+opacity:1,
+scale:1,
+duration: 1.5,
+  scrollTrigger: {
+    trigger: "#main",
+    start: "top top",
+    end: "+=" + window.innerHeight * 1,
+    scrub: 1,
+  }
+});
+gsap.to('#logo1', {
+opacity:0,
+scale:0,
+duration: 1,
+  scrollTrigger: {
+    trigger: "#main",
+    start: "top top",
+    end: "+=" + window.innerHeight * 1,
+    scrub: 1,
+  }
+});
+
 gsap.to('#nav', {
-          backgroundColor: 'rgb(49,23,114)',
-          color: 'white',
-          duration: 0.5,  // Adjust duration to make it faster
-          scrollTrigger: {
-            trigger: "#page1",
-            start: "top top",
-            end: "+=" + window.innerHeight * .2,
-            scrub: .0000001,
-          }
-        });
-        
+  backgroundColor: 'rgb(49,23,114)',
+  color: 'white',
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#page1",
+    start: "top top",
+    end: "+=" + window.innerHeight * .2,
+    scrub: .0000001,
+  }
+});
+
 gsap.to('#nav button', {
-          backgroundColor: 'rgb(49,23,114)',
-          color: 'white',
-          opacity:1,
-          duration: 0.5,  // Adjust duration to make it faster
-          scrollTrigger: {
-            trigger: "#page1",
-            start: "top top",
-            end: "+=" + window.innerHeight * .2,
-            scrub: .0000001,
-          }
-        });
-        
-        
-        
+  backgroundColor: 'rgb(49,23,114)',
+  color: 'white',
+  opacity: 1,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#page1",
+    start: "top top",
+    end: "+=" + window.innerHeight * .2,
+    scrub: .0000001,
+  }
+});
+
+
+
 gsap.to('.castle2', {
-          x: 150, // Animation property
-          scrollTrigger: {
-            trigger: "#page1",
-            start: "top 50",
-            end: "+=" + window.innerHeight * 2, // Dynamically adjust the end point
-          //   markers: true, // Enable debug markers
-            scrub: 1.2, // Smooth scrubbing
-          }
-        });  
+  x: 150,
+  scrollTrigger: {
+    trigger: "#page1",
+    start: "top 50",
+    end: "+=" + window.innerHeight * 2,
+    scrub: 1.2,
+  }
+});
 gsap.to('.castle2', {
-          x: 150, // Animation property
-          scrollTrigger: {
-            trigger: "#page1",
-            start: "top 50",
-            end: "+=" + window.innerHeight * 2, // Dynamically adjust the end point
-          //   markers: true, // Enable debug markers
-            scrub: 1.2, // Smooth scrubbing
-          }
-        });  
+  x: 150,
+  scrollTrigger: {
+    trigger: "#page1",
+    start: "top 50",
+    end: "+=" + window.innerHeight * 2,
+    scrub: 1.2,
+  }
+});
 gsap.to('#mountains', {
-          scale: 2.5, // Animation property
-          y:-30,
-          scrollTrigger: {
-            trigger: "#page1",
-            start: "top 50",
-            end: "+=" + window.innerHeight * 2, // Dynamically adjust the end point
-          //   markers: true, // Enable debug markers
-            scrub: 1.2, // Smooth scrubbing
-          }
-        });  
+  scale: 2.5,
+  y: -30,
+  scrollTrigger: {
+    trigger: "#page1",
+    start: "top 50",
+    end: "+=" + window.innerHeight * 2,
+    scrub: 1.2,
+  }
+});
 gsap.to('#page1', {
-          y:-1200,
-          scrollTrigger: {
-            trigger: "#page1",
-            start: "top top   ",
-            end: "+=" + window.innerHeight * 2, // Dynamically adjust the end point
-            scrub: 1, // Smooth scrubbing
-            marker:true
-          }
-        });  
-        
+  y: -1200,
+  scrollTrigger: {
+    trigger: "#page1",
+    start: "top top   ",
+    end: "+=" + window.innerHeight * 2, // D
+    scrub: 1,
+    marker: true
+  }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
-  // Select all buttons within the element1 div
   const buttons = document.querySelectorAll(".element1 button");
 
   // Apply GSAP animation to each button
   gsap.from(buttons, {
-    x: -200, // Starting position from the left
-    opacity: 0, // Starting with invisible
-    stagger: 0.2, // Time delay between animations of each button
-    duration: 1, // Animation duration for each button
-    ease: "power2.out", // Easing function for a smooth effect
+    x: -200,
+    opacity: 0,
+    stagger: 0.2,
+    duration: 1,
+    ease: "power2.out",
     scrollTrigger: {
       trigger: "#page2",
       start: "top top   ",
-      end: "+=" + window.innerHeight * 2, // Dynamically adjust the end point
-      // marker:true
+      end: "+=" + window.innerHeight * 2,
     }
   });
 });
@@ -134,16 +123,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".element2 button");
 
   gsap.from(buttons, {
-    x: 200, 
-    opacity: 0, // Starting with invisible
-    stagger: 0.2, // Time delay between animations of each button
-    duration: 1, // Animation duration for each button
-    ease: "power2.out", // Easing function for a smooth effect
+    x: 200,
+    opacity: 0,
+    stagger: 0.2,
+    duration: 1,
+    ease: "power2.out",
     scrollTrigger: {
       trigger: "#page2",
       start: "top top   ",
-      end: "+=" + window.innerHeight * 2, // Dynamically adjust the end point
-      // marker:true
+      end: "+=" + window.innerHeight * 2,
     }
   });
 });
